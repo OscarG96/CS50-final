c66eaf6ecc736a8c17ff669c0023f55a9410dded
Oscar G <oscar.gtzv@hotmail.com>
2021-09-29 23:50:03 -0500

    render sql query

--- flaskr/main.py
+++ flaskr/main.py
@@ -9,6 +9,7 @@ from flaskr.db import get_db
 
 bp = Blueprint('main', __name__)
 
+
 @bp.route("/")
 def index():
     return render_template("index.html")
@@ -16,12 +17,23 @@ def index():
 
 @bp.route("/movies", methods=('GET', 'POST'))
 def movies():
+    MOVIES = []
     if request.method == 'POST':
         searchField = request.form['searchField']
         searchValue = request.form['searchValue']
+        db = get_db("DATABASE_MOVIES")
+        error = None
+        movies = db.execute(
+            'SELECT * FROM movies WHERE title = ?', (searchValue,)
+        ).fetchone()
 
+        MOVIES.append(movies)
+        
         print(f"field", searchField)
         print(f"value", searchValue)
+        for field in movies:
+            print(f"movies sql", field)
+        return render_template("movies.html", movies=MOVIES)
 
     return render_template("movies.html")
     
