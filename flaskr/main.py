8ee8567f72539596510505bc1ecd04dc4213bf51
Oscar G <oscar.gtzv@hotmail.com>
2021-10-12 00:05:02 -0500

    added more fields to search form

--- flaskr/main.py
+++ flaskr/main.py
@@ -19,20 +19,22 @@ def index():
 def movies():
     MOVIES = []
     if request.method == 'POST':
-        searchField = request.form['searchField']
-        searchValue = request.form['searchValue']
+        for el in request.form:
+            print(f"field", el, request.form[el])
+        # searchField = request.form['searchField']
+        # searchValue = request.form['searchValue']
         db = get_db("DATABASE_MOVIES")
         error = None
-        movies = db.execute(
-            'SELECT * FROM movies WHERE title = ?', (searchValue,)
-        ).fetchone()
+        # movies = db.execute(
+        #     'SELECT * FROM movies WHERE title = ?', (searchValue,)
+        # ).fetchone()
 
-        MOVIES.append(movies)
+        # MOVIES.append(movies)
         
-        print(f"field", searchField)
-        print(f"value", searchValue)
-        for field in movies:
-            print(f"movies sql", field)
+        # print(f"field", searchField)
+        # print(f"value", searchValue)
+        # for field in movies:
+        #     print(f"movies sql", field)
         return render_template("movies.html", movies=MOVIES)
 
     return render_template("movies.html")
